2d1932b810f2f0f33bc51d306facc5392b1ef87c
Bram Mulders <bram.mulders@itility.nl>
2021-10-09 10:26:27 +0200

    Replace

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
@@ -18,7 +18,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/i,
+                test: /.css$/i,
                 use: ["style-loader", "css-loader"],
             },
         ],
